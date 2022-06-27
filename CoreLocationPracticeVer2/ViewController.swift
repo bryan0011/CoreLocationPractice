b06fd03dbb8ab68a61d0afc437318967776f239b
Bryan Kuo <bryan0011@gmail.com>
2022-06-28 00:07:14 +0800

    Final

--- CoreLocationPracticeVer2/ViewController.swift
+++ CoreLocationPracticeVer2/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func getLocation(_ sender: UIButton) {
+        
+        locationManager.requestLocation()
+        
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last!.coordinate
+        print(location.longitude)
+        locationLabel.text = "目前位置為\n經度為\(location.longitude)\n緯度為\(location.latitude)"
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("fail")
+    }
+}
