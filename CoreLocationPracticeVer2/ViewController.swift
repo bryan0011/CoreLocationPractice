ecc61d848dcd5c56f0df80fcd1e13394790dafd9
Bryan Kuo <bryan0011@gmail.com>
2022-06-27 16:00:45 +0800

    Initial Commit

---
+++ CoreLocationPracticeVer2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationPracticeVer2
+//
+//  Created by Bryan Kuo on 2022/6/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
